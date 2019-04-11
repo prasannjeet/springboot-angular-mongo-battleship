d6c6b425c3b4a319275598912e22c994699babb4
prasannjeet <prasannjeetsingh@gmail.com>
2019-04-11 11:48:49 +0200

    added routing for table

--- battleship-front/src/app/app-routing.module.ts
+++ battleship-front/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {BattleboardComponent} from './components/battleboard/battleboard.component';
 import {Battleboard2Component} from './components/battleboard2/battleboard2.component';
+import {ScoreTableComponent} from './components/score-table/score-table.component';
 
 const routes: Routes = [{
     path: 'battleboard',
@@ -9,6 +10,9 @@ const routes: Routes = [{
 }, {
     path: 'playWithFriend/:userId/:socketUrl',
     component: Battleboard2Component
+}, {
+    path: 'scoreTable',
+    component: ScoreTableComponent
 }];
 
 @NgModule({
