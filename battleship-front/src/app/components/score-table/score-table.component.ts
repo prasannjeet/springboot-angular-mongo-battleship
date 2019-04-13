c69d8d524c6d825e0b80f277df6f84f6986cf465
prasannjeet <prasannjeetsingh@gmail.com>
2019-04-13 04:30:26 +0200

    display lost games too

--- battleship-front/src/app/components/score-table/score-table.component.ts
+++ battleship-front/src/app/components/score-table/score-table.component.ts
@@ -14,19 +14,23 @@ export class ScoreTableComponent implements OnInit {
 
     names = [];
     winCounts = [];
+    lostCounts = [];
     randomColors = [];
 
     ngOnInit() {
 
 
         this.battleService.getAllPlayerData().subscribe(data => {
+            console.log(data);
             // console.log(data);
             for (const i in data) {
                 this.names.push(i); // alerts key
-                this.winCounts.push(data[i]); //alerts key's value
+                this.winCounts.push(data[i][0]); //alerts key's value (wincount)
+                this.lostCounts.push(data[i][1]); //alerts other key value (lostcount)
                 this.randomColors.push(this.getRandomColor());
             }
             console.log(this.names);
+            console.log(this.lostCounts);
             console.log(this.winCounts);
         }, error => {
             console.error(error);
